0981bc9716904dcc5ccd3e1bf1dc92bd01619938
erinspace <erin.braswell@gmail.com>
2015-10-28 09:41:15 -0400

    Add inv tasks for calling the orcid API

--- tasks.py
+++ tasks.py
@@ -1,7 +1,21 @@
 import os
 from invoke import task
 
-from enshance.tasks import gather_contributors
+from enshance.tasks import gather_contributors, gather_email_orcid, gather_email_from_orcid
+
+
+@task
+def get_email_orcid(start=None, async=False):
+    from enshance import settings
+    settings.CELERY_ALWAYS_EAGER = not async
+    gather_email_orcid(start).delay()
+
+
+@task
+def get_email_from_orcid(start=None, async=False):
+    from enshance import settings
+    settings.CELERY_ALWAYS_EAGER = not async
+    gather_email_from_orcid(start).delay()
 
 
 @task
