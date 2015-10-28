9adb49f712060435b823898167e55682defa7b80
erinspace <erin.braswell@gmail.com>
2015-10-28 09:40:47 -0400

    Add celery settings to init

--- enshance/settings/__init__.py
+++ enshance/settings/__init__.py
@@ -8,3 +8,11 @@ try:
 except ImportError as error:
     logger.warn("No enshance local.py settings file found. Try running $ cp enshance/enshance/settings/local-dist.py enshance/enshance/settings/local.py. Defaulting to enshance/settings/local.py")
     from enshance.settings.local import *
+
+
+CELERY_ENABLE_UTC = True
+CELERY_RESULT_BACKEND = None
+CELERY_TASK_SERIALIZER = 'pickle'
+CELERY_ACCEPT_CONTENT = ['pickle']
+CELERY_RESULT_SERIALIZER = 'pickle'
+CELERY_IMPORTS = ('enshance.tasks')
